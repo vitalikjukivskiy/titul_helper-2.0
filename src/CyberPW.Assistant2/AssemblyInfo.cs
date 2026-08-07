f096c59ff2b22d89d1750acbc7c0984ec95db3a1
vitalikjukivskiy <vitaliy.golovnya1@gmail.com>
2026-08-07 17:20:52 +0300

    chore: sync assembly version with VERSION

--- src/CyberPW.Assistant2/AssemblyInfo.cs
+++ src/CyberPW.Assistant2/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("CyberPW Assistant")]
 [assembly: AssemblyCopyright("CyberPW Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.12.0")]
-[assembly: AssemblyFileVersion("2.0.12.0")]
+[assembly: AssemblyVersion("2.0.13.0")]
+[assembly: AssemblyFileVersion("2.0.13.0")]
